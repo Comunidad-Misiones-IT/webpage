fed42ebbb2586943423bd9f52d4e49e7235ef786
Nataya Soledad <natayafs@gmail.com>
2020-10-03 14:10:42 -0300

    Revert "Revert "Merge branch 'master' into nata-branch""
    This reverts commit eecd5046c13dab4631e7fed8710ed40cd7a1ae0b.

---
+++ src/components/CardList.vue
@@ -0,0 +1,60 @@
+<template>
+	<div class="links container-fluid">
+		<Card 
+		v-for="(card, index) in cards"
+		v-bind="card"
+		:key="index" />
+	</div>
+</template>
+
+<script>
+	import Card from './Card';
+
+	export default {
+		components: {
+			Card
+		},
+		data () {
+			return {
+				cards: [
+					{
+						nombre: "Discord",
+						img_src: "/img/discord.png",
+						link_src: "https://discord.com/invite/6Tkv8pn"
+					},
+					{
+						nombre: "Instagram",
+						img_src: "/img/instagram.png",
+						link_src: "https://www.instagram.com/comunidadmit/"
+					},
+					{
+						nombre: "Twitter",
+						img_src: "/img/twitter.png",
+						link_src: "https://twitter.com/ComunidadMIT"
+					},
+					{
+						nombre: "LinkedIn",
+						img_src: "/img/linkedin.png",
+						link_src: "https://www.linkedin.com/company/comunidad-mit/"
+					},
+					{
+						nombre: "Github",
+						img_src: "/img/github.png",
+						link_src: "https://github.com/Comunidad-Misiones-IT"
+					},
+					{
+						nombre: "WEBINAR: Programar desde Misiones",
+						img_src: "/img/youtube.png",
+						link_src: "https://www.youtube.com/watch?v=tabBLNTPInQ"
+					}
+				]
+			}
+		}
+	}
+</script>
+
+<style>
+	.links {
+		padding: 0.2em 1.6em;
+	}
+</style>
