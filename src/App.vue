e131f71edbfe44148262639677f25f2e2f3a1583
Nataya Soledad <natayafs@gmail.com>
2020-10-03 13:54:50 -0300

    Revert "Merge pull request #3 from Comunidad-Misiones-IT/nata-branch"
    This reverts commit acebe83b821137db702259f3849d1c2bb7800ba1, reversing
    changes made to 2391b18829716864127700107900c36f2ffc4884.

--- src/App.vue
+++ src/App.vue
@@ -1,63 +1,5 @@
 <template>
-  <div id="app" class="container-fluid"
-    <div class="mb-2">
-      <button type="button" class="btn" onclick="cambiarModo()">🌞|🌚</button>
-    </div>
-
-    <img :src="avy" class="avatar" id="img" alt="avatar" />
-    <h1>{{ msg }}</h1>
-
-  <div class="desc">
-    <p>Somos una comunidad convencida de que Misiones tiene talento valioso para la industria IT, y lo queremos potenciar. Si queres saber que hay detrás de esta pagina, manijear juegos o compartir experiencias seguí a MIT en sus redes:</p>
-  </div>
- 
-    <div class="links container-fluid">
-      <div class="link" id="link">
-        <a href="https://discord.com/invite/6Tkv8pn">
-          <img :src="Discord" class="ico" id="ico" alt="discord" />
-          Discord
-        </a>
-      </div>
-    
-      <div class="link" id="link">
-        <a href="https://www.instagram.com/comunidadmit/">
-          <img :src="Instagram" class="ico" id="ico" alt="instagram" />
-          <a>Instagram</a>
-        </a>
-      </div>   
-      
-      <div class="link" id="link">
-        <a href="https://twitter.com/ComunidadMIT">
-          <img :src="Twitter" class="ico" id="ico" alt="twitter" />
-          Twitter
-        </a>
-      </div>
-
-      <div class="link" id="link">
-        <a href="https://www.linkedin.com/company/comunidad-mit/">
-          <img :src="Linkedin" class="ico" id="ico" alt="linkedin" />
-          Linkedin
-        </a>
-      </div>
-
-      <div class="link" id="link">
-        <a href="https://github.com/Comunidad-Misiones-IT">
-          <img :src="Github" class="ico" id="ico" alt="github" />
-          Github
-        </a>
-      </div>
-
-      <div class="link" id="link">
-          <a href="https://www.youtube.com/watch?v=tabBLNTPInQ">
-            <img :src="Youtube" class="ico" id="ico" alt="youtube" />
-           WEBINAR: Programar desde Misiones
-          </a>
-      </div>
-    </div>
-
-    <div class="footer container-fluid pt-3 pb-1">
-      <sup>All rigths reserved | Comunidad MIT © 2020</sup>
-    </div>
+  <div id="app" class="container-fluid">
 
     <Header />
     <CardList />
@@ -103,49 +45,6 @@ export default {
 .oscuro { 
     background-color: #1f1f1f; 
     color: #f1eded;
-}
-
-@media screen and (min-width: 650px) {
-  .links {
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
-  }
-
-  .link {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    height: 160px;
-    width: 90%;
-    border-radius: 15px;
-    padding: 3em;
-    background-color: transparent;
-  }
-
-  .ico {
-    width: 50px;
-  }
-
-  a {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-  }
-
-  .desc {
-    margin: 4em;
-  }
-}
-
-@media screen and (max-width: 650px) { 
-  .ico {
-    width: 30px;
-  }
-
-  .desc {
-    margin: 2em;
-  }
 }
 
 .oscuro .img { 
